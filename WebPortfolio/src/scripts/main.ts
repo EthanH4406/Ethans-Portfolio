5b66326aee61d853d8f26466e4e38f791c85277c
Ethan <itsethanhook@gmail.com>
2026-02-04 16:27:31 -0800

    Removed Legacy Portfolio System
    Removed Portfolio Card/Github system in prep for a newer, simpler approach

--- WebPortfolio/src/scripts/main.ts
+++ WebPortfolio/src/scripts/main.ts
@@ -1,33 +1,10 @@
 import '../styles/main.scss';
 
-import PortfolioApp from './PortfolioApp';
-import '../styles/components/_portfolio.scss'; // Don't forget the portfolio styles!
 import './HomePageScripts';
 
 const currentPage = document.body.dataset.page;
 
 if(currentPage==='portfolio') {
     import('./PortfolioScripts');
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const GITHUB_TOKEN = '';
-
-        const app = new PortfolioApp('portfolio-projects', GITHUB_TOKEN);
-
-        const projectFiles = [
-            './RepoMarkdownFiles/JunkyardJunction.md',
-            './RepoMarkdownFiles/StreetPainter.md',
-            './RepoMarkdownFiles/TerraEngine.md',
-            './RepoMarkdownFiles/PortfolioWebsite.md'
-        ];
-
-        app.init(projectFiles)
-            .then(() => {
-                console.log('Portfolio loaded successfully');
-            })
-            .catch(error => {
-                console.error('Error loading portfolio', error);
-            });
-    });
 }
 
